ed96e54b0cf01d36a3ec7ca04c0be439c689b752
VoQuangTan360 <80733265+VoQuangTan360@users.noreply.github.com>
2022-08-05 18:17:10 +0700

    call phone

--- app/src/main/java/com/intern/evtutors/MainActivity.kt
+++ app/src/main/java/com/intern/evtutors/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.intern.evtutors
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.intern.evtutors.call
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        itemcall.setOnClickListener(View.OnClickListener { View ->
+            val intent = Intent(this, call::class.java)
+            startActivity(intent)
+        })
     }
 }
